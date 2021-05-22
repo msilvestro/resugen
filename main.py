c8ef84832e3609c91f7fa0be40ad4460c69def03
Matteo Silvestro <matteosilvestro@live.it>
2021-05-22 11:42:46 +0200

    Read content from yaml resume

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from jinja2 import Environment, FileSystemLoader
+import yaml
 
 TEMPLATE_NAME = "ginseng"
 
 template_loader = FileSystemLoader(searchpath="./template")
 template_env = Environment(loader=template_loader)
 template = template_env.get_template(f"{TEMPLATE_NAME}.html")
-outputText = template.render()
 
+with open("resume.yml", "r") as input_file:
+    resume = yaml.safe_load(input_file)
 with open("output/resume.html", "w") as output_file:
-    print("writing!")
-    output_file.write(template.render(name="Matteo Silvestro"))
+    output_file.write(template.render(resume=resume))
